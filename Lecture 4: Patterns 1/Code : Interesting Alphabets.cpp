198382d67d5b4cbae278c0098260e6bc1903ad9b
Sugam Arora <96546088+SUGAM-ARORA@users.noreply.github.com>
2022-11-27 14:35:16 +0530

    Create Code : Interesting Alphabets.cpp

---
+++ Lecture 4: Patterns 1/Code : Interesting Alphabets.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+int n;
+    cin >> n;
+    int i=1;
+        while (i<=n) {
+            int j=1;
+            char startchar='D'+n-i-3;
+                while (j<=i) {
+                	char ch=startchar+j-1;
+             cout << ch;
+                j++;
+                }
+            cout << endl;
+            i++;
+        }
+
+}
