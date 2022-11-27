bcca927f584dc9e217d20af70efcc4c5c6ad88cf
Sugam Arora <96546088+SUGAM-ARORA@users.noreply.github.com>
2022-11-27 14:59:55 +0530

    Create Print All Substrings.cpp

---
+++ Lecture 10: Character Arrays and 2D Arrays/Print All Substrings.cpp
@@ -0,0 +1,10 @@
+void printSubstrings(char str[]){
+    for(int start = 0; str[start] != '\0'; start++){
+        for(int end = start; str[end] != '\0'; end++){
+            for(int i = start; i <= end; i++){
+                cout << str[i];
+            }
+            cout << endl;
+        }
+    }
+}
