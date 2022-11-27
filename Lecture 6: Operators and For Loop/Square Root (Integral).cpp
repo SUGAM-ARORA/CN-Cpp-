d2c327b5ce7c821504a0499886f7fdf0197ac862
Sugam Arora <96546088+SUGAM-ARORA@users.noreply.github.com>
2022-11-27 14:43:34 +0530

    Create Square Root (Integral).cpp

---
+++ Lecture 6: Operators and For Loop/Square Root (Integral).cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int output = 0;
+    while(output * output <= n) {
+        output = output + 1;
+    }
+    output = output - 1;
+    cout << output;
+}
