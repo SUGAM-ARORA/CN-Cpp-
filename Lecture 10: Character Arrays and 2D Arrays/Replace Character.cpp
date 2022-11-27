cfa7e81e8d93e5e2da57ba68d65a5448cff32809
Sugam Arora <96546088+SUGAM-ARORA@users.noreply.github.com>
2022-11-27 14:58:42 +0530

    Create Replace Character.cpp

---
+++ Lecture 10: Character Arrays and 2D Arrays/Replace Character.cpp
@@ -0,0 +1,9 @@
+void replaceCharacter(char input[], char c1, char c2) {
+    int i=0;
+    int j = strlen(input);
+    for(i; i < j;i++){
+        if(input[i] == c1){
+            input[i]=c2;
+        }
+    }
+}
