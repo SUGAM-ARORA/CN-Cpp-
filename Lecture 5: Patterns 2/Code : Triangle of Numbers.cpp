f07dce3b1e13931cacc7d33729a6094f5bc76869
Sugam Arora <96546088+SUGAM-ARORA@users.noreply.github.com>
2022-11-27 14:38:14 +0530

    Create Code : Triangle of Numbers.cpp

---
+++ Lecture 5: Patterns 2/Code : Triangle of Numbers.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+int n;
+    cin >> n;
+    int i=1;
+        while (i<=n) {
+            int spaces=1;
+        while (spaces<=n-i) {
+        	cout << " ";
+        	spaces=spaces+1;
+	}
+	int j=1;
+    while (j<=i) {
+    cout << i+j-1 ;
+        j=j+1;
+				}
+		j=i-1;
+		while (j>=1) {
+			cout << i+j-1;
+			j=j-1;
+		}		
+		cout << endl;
+            i=i+1;
+        }
+}
